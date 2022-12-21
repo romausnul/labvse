400a5561860e4ae56bc5f25388c270273caa6bcf
РомаН <119739400+romausnul@users.noreply.github.com>
2022-12-21 23:17:16 +0300

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ответьте на 5 вопросов получишь игоря");
+            double a = 0;
+            Console.WriteLine("Назавите устройства вывода");
+            string y = Convert.ToString(Console.ReadLine());
+            if (y == "Принтер")
+            {
+                Console.WriteLine("ОПА ПРАВИЛЬНО");
+                a = a + 1;
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ответ");
+            }
+
+            Console.WriteLine("Ростелеком топ?");
+            y = Convert.ToString(Console.ReadLine());
+            if (y == "да")
+            {
+                Console.WriteLine("Правильный ответ");
+                a = a + 1;
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ответ");
+            }
+
+            Console.WriteLine("Ты пользуешься интернетем?");
+            y = Convert.ToString(Console.ReadLine());
+            if (y == "да")
+            {
+                Console.WriteLine("Правильный ответ");
+                a = a + 1;
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ответ");
+            }
+
+            Console.WriteLine("У тебя есть пк?");
+            y = Convert.ToString(Console.ReadLine());
+            if (y == "нет")
+            {
+                Console.WriteLine("Правильный ответ");
+                a = a + 1;
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ответ");
+            }
+
+            Console.WriteLine("Ты играешьв танки?");
+            y = Convert.ToString(Console.ReadLine());
+            if (y == "нет")
+            {
+                Console.WriteLine("Правильный ответ");
+                a = a + 1;
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ответ");
+            }
+            Console.WriteLine($"Правильных ответов: {a}");
+            double b = Convert.ToDouble((a / 5) * 100);
+            Console.WriteLine($"Точность ответов: {b}%");
+            if (b <= 40)
+                Console.WriteLine("У тебя 2");
+            if (b >= 50) || (b<=70)
+                Console.WriteLine("Опа моя оценка  3");
+            if (b >=80) || (b<=90)
+                Console.WriteLine("ТЫ не отчислен значит 4 4");
+            if ()
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ВОПРОСЫ, ОТВЕТЫ/ВОПРОСЫ, ОТВЕТЫ/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ВОПРОСЫ__ОТВЕТЫ
+{
+    class Program
+    {
+        private const string V = "Проц";
+
+        static void Main()
+            {
+            next:
+                Random question = new Random();
+                int x = question.Next(5);
+                string[] array = new string[5];
+                array[0] = "Устройства ввода информации";
+                array[1] = "Устройства вывода информации";
+                array[2] = "Обратка информации";
+                array[3] = "Какие есть ОС? перечислить два штука";
+                array[4] = "Перечислите составляющие ПК";
+
+               Console.WriteLine(array[x]);
+
+                string[] answer = new string[5];
+                answer[0] = "Мышь";
+                answer[1] = "Принтер";
+                answer[2] = "Проц";
+                answer[3] = "Винда";
+                answer[4] = "Материнская плата";
+                
+            string a = Console.ReadLine();
+
+                if (a == answer[x])
+                {
+                    Console.WriteLine("Ты помоему правильно  \n*Оценка 4:");
+                    goto next;
+                }
+                else
+                    Console.WriteLine("Ты вроде попутал \n*НУ пж попробуй ещё раз.");
+
+                Console.ReadLine();
+            }
+    }
+}

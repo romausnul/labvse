93c19e4f7ec2ad08977e9417c9b892f03bcb6e8f
РомаН <119739400+romausnul@users.noreply.github.com>
2023-03-23 13:05:06 +0300

    Add files via upload

---
+++ Лаба 6 2/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите a");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите c");
+double c = Convert.ToDouble(Console.ReadLine());
+double d = a;
+while (d < b)
+{
+    if (d % c == 0)
+    {
+        Console.WriteLine($"Число, кратное C = {d}");
+    }
+    d += 1;
+
+}
---
+++ лаба 6/лаба 6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            while (a < 1000)
+            {
+                if ((a % 47) == 43)
+                    Console.WriteLine($"Остаток от деления {a}");
+                a += 1;
+            }
+            a = 100;
+            while (a < 1000)
+            {
+                if ((a % 43) == 47)
+                    Console.WriteLine($"Остаток от деления {a}");
+                a += 1;
+            }
+            Console.WriteLine("нет остатка 47 от деления на 43");
+            Console.ReadKey();
+        }
+    }
+}

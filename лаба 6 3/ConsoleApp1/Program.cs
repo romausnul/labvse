bb194f715335d35d8f2f01095b3debc0062aa9bf
РомаН <119739400+romausnul@users.noreply.github.com>
2023-03-23 13:03:37 +0300

    Add files via upload

---
+++ лаба 6 3/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите возраст внука");
+double v = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите возраст дедушки");
+double d = Convert.ToDouble(Console.ReadLine());
+while (d != 2 * v)
+{
+    v = v + 1;
+    d = d + 1;
+}
+Console.WriteLine($"Возарст внука: {v}");
+Console.WriteLine($"Возарст дедушки: {d}");
---
+++ лаба 6 4/ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+while (a > 0)
+{
+    a = a / 10;
+    b = b + 1;
+}
+Console.WriteLine($"В этом числе {b} цифр");
+

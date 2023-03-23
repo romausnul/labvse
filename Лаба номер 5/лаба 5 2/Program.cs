ac7d654c354407cef8782c379b388c5ec24dd2ef
РомаН <119739400+romausnul@users.noreply.github.com>
2023-03-23 13:01:45 +0300

    Add files via upload

---
+++ Лаба номер 5/лаба 5 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace лаба_5_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 9;
+            int count = 0;
+            for (int i = 100; i < 1000; ++i)
+            {
+                string str = i.ToString();
+
+                if (int.Parse(str[0].ToString()) + int.Parse(str[1].ToString()) + int.Parse(str[2].ToString()) == n)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Результат: {0}", count);
+        }
+    }
+}

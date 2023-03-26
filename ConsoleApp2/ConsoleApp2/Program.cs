8e0750e7506116a1b925b945e616a9f1cd8959a3
РомаН <119739400+romausnul@users.noreply.github.com>
2023-03-26 21:10:44 +0300

    Add files via upload

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,31 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp5
+namespace ConsoleApp2
+{
+    internal class Program
     {
-        class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            int kolTovar1 = 0;
+            int kolTovar2 = 0;
+            int kolTovar3 = 0;
+            int costTovar1 = 1488;
+            int costTovar2 = 1337;
+            int costTovar3 = 228;
+            int deng = 0;
+            int realitog = 0;
+
+            Console.WriteLine("Введите название первого товара");
+            string tovar1 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите кол-во товара");
+            kolTovar1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите название второго товара");
+            string tovar2 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите кол-во товара");
+            kolTovar2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите название третьего товара");
+            string tovar3 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите кол-во товара");
+            kolTovar3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Внесите сумму денег");
+            deng = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Чек:");
+            int itog = (kolTovar1 * costTovar1) + (kolTovar2 * costTovar2) + (kolTovar3 * costTovar3);
+            Console.WriteLine(itog);
+            Console.WriteLine("Внесенная сумма");
+            Console.WriteLine(deng);
+
+            realitog = itog - deng;
+            
+            if (realitog == 0)
             {
-                int e;
-                int r;
-
-                for (int i = 0; i < 21; i++)
-                {
-                    r = 1;
-                    e = i;
-
-                    while (e > 0)
-                    {
-                        r *= 2;
-                        e--;
-                    }
-                    Console.WriteLine("2 в степени " + i + " равно " + r);
-                }
-                Console.ReadKey();
+                Console.WriteLine("Спасибо за покупку");
             }
+            if (realitog > 0)
+            {
+
+            }
+
         }
- }
-
-
-
+    }
+}
--- ConsoleApp2/ConsoleApp2/Properties/AssemblyInfo.cs
+++ ConsoleApp2/ConsoleApp2/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ConsoleApp2")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("511fec07-5881-4a9b-97c9-fc4c93d8ed32")]
+[assembly: Guid("837b97b3-36e7-4bee-b1b6-00592517bcda")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //

dd9f400556e018a99556f6b3d6bfa374849d79c3
РомаН <119739400+romausnul@users.noreply.github.com>
2023-05-26 09:16:44 +0300

    Add files via upload

---
+++ 19 лаба/19 лаба/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+foreach (var drive in DriveInfo.GetDrives())
+{
+    try
+    {
+        Console.WriteLine("Имя: " + drive.Name);
+        Console.WriteLine("Файловая система: " + drive.DriveFormat);
+        
+        Console.WriteLine("ТИП ДИСКА: " + drive.DriveType);
+       
+        Console.WriteLine("Сколько свободного места (в байтах): " + drive.AvailableFreeSpace);
+        
+        Console.WriteLine("Готов диск: " + drive.IsReady);
+        
+        Console.WriteLine("Корневой каталог диска: " + drive.RootDirectory);
+       
+        Console.WriteLine("Сколько доступного на диске (в байтах): " + drive.TotalFreeSpace);
+       
+        Console.WriteLine("Размер диска (в байтах): " + drive.TotalSize);
+        
+        Console.WriteLine("Метка тома диска: " + drive.VolumeLabel);
+    }
+    catch { }
+
+    Console.WriteLine();
+}
